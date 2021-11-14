90a8b66077a56388f20d8e65ef3857f63dbcbf3a
jingshing001 <a2324395@gmail.ocm>
2021-11-14 15:31:37 +0800

    連接Database

---
+++ nodeProject/config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:"mongodb+srv://jingshiang:a2389101@cluster0.niltd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- nodeProject/server.js
+++ nodeProject/server.js
@@ -1,7 +1,15 @@
 const express=require('express');
+const mongoose = require('mongoose');
+const db=require('./config/keys').mongoURI;
 const app=express();
 const port=process.env.PORT || 3000;
 
+mongoose.connect(db, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  }).then(()=>{
+        console.log("MONGODB CONNECTED")
+  }).catch(err=>console.log(err));
 
 app.get('/',((req,res)=>{
         res.send('HELLO');
