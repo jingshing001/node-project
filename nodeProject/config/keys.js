585a9fa55ec970c9de6e1dbeb57f38c038219a90
jingshing001 <a2324395@gmail.ocm>
2021-11-16 16:31:02 +0800

    搭建登入路由設置token

--- nodeProject/config/keys.js
+++ nodeProject/config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI:"mongodb+srv://jingshiang:a2389101@cluster0.niltd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://jingshiang:a2389101@cluster0.niltd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrkey:"secret"
 }
--- nodeProject/routes/api/users.js
+++ nodeProject/routes/api/users.js
@@ -2,6 +2,8 @@ const express=require("express");
 const User=require('../../models/User');
 const bcrypt=require("bcrypt");
 const gravatar=require('gravatar');
+const jwt=require('jsonwebtoken');
+const keys=require("../../config/keys")
 const router=express.Router();
 
 
@@ -11,7 +13,7 @@ router.get("/test",(req,res)=>{
     res.json({msg:"login works"});
 })
 
-//登路路由
+//註冊路由
 router.post('/register',((req,res)=>{
     console.log(req.body);
     User.findOne({email:req.body.email})
@@ -41,4 +43,37 @@ router.post('/register',((req,res)=>{
             }
         })
 }))
+
+//登入路由
+router.post('/login',(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+    //查詢數據庫
+    User.findOne({email})
+        .then((user)=>{
+            if(!user){
+                return res.status(404).json({email:'用戶不存在!'})
+            }
+
+            //密碼配對
+            bcrypt.compare(password,user.password)
+                  .then(isMatch=>{
+                      if(isMatch){
+                        const rule={id:user.id,name:user.name}
+                        jwt.sign(rule,keys.secretOrkey,{expiresIn:3600},(err,token)=>{
+                            if(err)throw err;
+                            res.json({
+                                success:true,
+                                token:"mrwu"+token
+                            })
+                        })
+                        // res.json({msg:"success"});
+                      }else{
+                            return res.status(400).json({password:"密碼錯誤"});
+                      }
+                  }) 
+        })
+})
+
+
 module.exports= router;
