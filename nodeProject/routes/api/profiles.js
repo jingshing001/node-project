a6a89e087c2a413f746b2abedef9b906f6fef6ee
jingshing001 <a2324395@gmail.ocm>
2021-11-17 19:05:14 +0800

    創建新增和查詢接口

--- nodeProject/routes/api/profiles.js
+++ nodeProject/routes/api/profiles.js
@@ -10,5 +10,45 @@ router.get('/test',(req,res)=>{
 
 //註冊路由
 
+//新增數據
+router.post('/add',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    const profileFields={};
+    if(req.body.type) profileFields.type=req.body.type;
+    if(req.body.describe) profileFields.describe=req.body.describe;
+    if(req.body.income) profileFields.income=req.body.income;
+    if(req.body.expend) profileFields.expend=req.body.expend;
+    if(req.body.cash) profileFields.cash=req.body.cash;
+    if(req.body.remark) profileFields.remark=req.body.remark;
+
+    new Profile(profileFields).save().then((profile)=>{
+        res.json(profile)
+    })
+})
+
+//獲取所有數據
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.find()
+    .then((profile)=>{
+        if(!profile){
+            return res.status(404).json("沒有任何內容");
+        }
+        res.json(profile);
+    })
+    .catch(err=>res.status(404).json(err));            
+})
+
+//獲取特定數據
+router.get('/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOne({_id:req.params.id})
+    .then((profile)=>{
+        if(!profile){
+            return res.status(404).json("沒有任何內容");
+        }
+        res.json(profile);
+    })
+    .catch(err=>res.status(404).json(err));            
+})
+
+
 
 module.exports= router;
