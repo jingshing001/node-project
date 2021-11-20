5bae7a3048c3a585b38e3a6cf062334732ca4d47
jingshing001 <a2324395@gmail.ocm>
2021-11-20 18:10:58 +0800

    登入頁面完成

--- nodeProject/vue-front/src/router/index.js
+++ nodeProject/vue-front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
 import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
 import NotFound from '../views/404.vue'
 
 
@@ -22,7 +23,13 @@ const routes = [
    path:'/register',
    name:'register',
    component:Register
- },{
+ },
+ {
+   path:'/login',
+   name:'login',
+   component:Login
+ },
+ {
    path:'*',
    name:'/404',
    component:NotFound
---
+++ nodeProject/vue-front/src/views/Login.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="login">
+        <section class="form_container">
+            <div class="manage_tip">
+                <span class="title">後臺管理系統</span>
+                <el-form :model="loginUser" :rules="rules" ref="loginForm" label-width="80px" class="loginForm">
+                        <el-form-item label="電子信箱" prop="email">
+                            <el-input v-model="loginUser.email" placeholder="請輸入email"></el-input>
+                        </el-form-item>
+                         <el-form-item  label="密碼" prop="password">
+                            <el-input v-model="loginUser.password" type='password' placeholder="請輸入密碼" ></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" class="submit_btn" @click="submitForm('loginForm')">登入</el-button>
+                        </el-form-item>
+                        <div class="goRegister">
+                            <p>還沒有帳號?現在<router-link to='/register'>註冊</router-link></p>
+                        </div>
+                </el-form>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login',
+    components:{},
+    data(){
+    return {
+            loginUser:{
+                email:'',
+                password:''
+            },
+            rules:{
+                email:[
+                    {
+                        type:"email",
+                        required:true,
+                        message:"電子信箱格式不正確",
+                        trigger:'blur'
+                    }
+                    ],
+                password:[
+                    {
+                        required:true,
+                        message:'密碼不能為空',
+                        trigger:'blur'
+                    },
+                    {
+                        min:6,
+                        max:30,
+                        message:'長度在6到30之間',
+                        trigger:'blur'
+                    }
+                    ]
+            }
+        }
+    },
+    methods:{
+        submitForm(formName){
+        this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.$axios.post("/api/users/login",this.loginUser)
+                    .then(res=>{
+                    //console.log(res);
+                    //token
+                    const { token }=res.data;
+                    localStorage.setItem('tokenId',token);
+                    this.$router.push('/index');
+                    })
+                 
+             }
+        });
+         
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .login{
+        position: relative;
+        width:100%;
+        height:100%;
+        background: url('../assets/bg.jpg') no-repeat center ; 
+        background-size:cover;
+    }
+
+    span{
+        margin-left: 55px;
+    }
+    
+    .form_container{
+        width:370px;
+        height:210px;
+        position: absolute;
+        top:10%;
+        left:34%;
+        border-radius: 5px;
+        text-align:center;
+    }
+
+    .form_container .manage_tip .title{
+        font-weight: bold;
+        font-size: 26px;
+        color:rgb(228, 147, 147);
+    }
+
+    .loginForm{
+        margin-top: 20px;
+        padding: 20px 40px 20px 20px;
+        border-radius: 5px;
+    }
+    
+    .submit_btn{
+        width:100%
+    }
+    
+    .goRegister{
+        font-size: 0.5rem;
+        float: right;
+        letter-spacing: 2px;
+    }
+     
+
+</style>
